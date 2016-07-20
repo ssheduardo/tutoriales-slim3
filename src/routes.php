c3e72f2698f2f606fc2367b406f182ac5d67cf70
eduardo <ssh.eduardo@gmail.com>
2016-07-20 22:55:39 +0200

    - Added routes and templates

---
+++ src/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+$app->get('/', function($request, $response, $args){
+	$this->logger->addInfo('In home');
+	return $response->write('Welcome to home');
+});
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    $response->write("Hello, " . $args['name']);
+    return $response;
+});
+
+$app->get('/home/[{name}]', function($request, $response, $args){
+
+	return $this->view->render($response,'index.phtml', $args);
+});
