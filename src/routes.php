3338ace7e8ef40450517eb73ffb677e96808840a
eduardo <ssh.eduardo@gmail.com>
2016-10-10 00:21:54 +0200

    - Added db

--- src/routes.php
+++ src/routes.php
@@ -16,3 +16,4 @@ $app->get('/home/[{name}]', function($request, $response, $args){
 });
 
 $app->get('/test','UserController:show')->setName('mt');
+$app->get('/users','UserController:users')->setName('users');
